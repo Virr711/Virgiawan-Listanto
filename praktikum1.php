2a2698dd142c448603bc70d8bc28a6fb90583e7b
Virr711 <virrarata@gmail.com>
2025-10-01 10:31:45 +0700

    Praktikum1

---
+++ praktikum1.php
@@ -0,0 +1,28 @@
+<html>
+    <title>Praktukum 1</title>
+<?php
+
+// Deret a: 4, 6, 9, 13, 18, ?, ?
+$a = [4, 6, 9, 13, 18];
+$diff = 2;
+for ($i = 5; $i < 7; $i++) {
+    $diff++;
+    $a[] = $a[$i-1] + $diff;
+}
+echo "Deret a: " . implode(", ", $a) . "\n","<br>";
+
+// Deret b: 2, 2, 3, 3, 4, ?, ?
+$b = [2, 2, 3, 3, 4];
+$b[] = 4;
+$b[] = 5;
+echo "Deret b: " . implode(", ", $b) . "\n","<br>";
+
+// Deret c: 1, 9, 2, 10, 3, ?, ?
+$c = [1, 9, 2, 10, 3];
+$c[] = 11;
+$c[] = 4;
+echo "Deret c: " . implode(", ", $c) . "\n","<br>";
+
+
+?>
+</html>
